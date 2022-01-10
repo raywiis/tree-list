abd5bc7a8952f4b6f4ad67e70930e2ac2e9cd1d8
Rėjus Paukštė <rejus@zenitech.co.uk>
2022-01-10 14:44:58 +0200

    Localise nodes per hook

--- src/useTree.tsx
+++ src/useTree.tsx
@@ -1,19 +1,21 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { Tree, TreeNode, TreeNodeId } from "./types";
 
-const nodes: Tree = new Map<TreeNodeId, TreeNode>([
-	[
-		0,
-		{
-			id: 0,
-			name: "Root",
-			children: [],
-		},
-	],
-]);
 
 export const useTree = () => {
 	const [idCounter, setIdCounter] = useState(1);
+
+	const nodes = useMemo(() => new Map<TreeNodeId, TreeNode>([
+		[
+			0,
+			{
+				id: 0,
+				name: "Root",
+				children: [],
+			},
+		],
+	]), []);
+
 	const addNode = useCallback(
 		(parentId: TreeNodeId, newNodeName: string) => {
 			const parentNode = nodes.get(parentId);
